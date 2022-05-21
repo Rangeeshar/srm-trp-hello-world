b601244b14ac83997c4d8efc6f0772d30387c86b
gifts0n <99631987+belikegt@users.noreply.github.com>
2022-05-21 20:39:52 +0530

    i guess ive created what youve asked  (#2)
    Giftson-21May

---
+++ Giftson/commandLineFunctionPassing.py
@@ -0,0 +1,25 @@
+"""
+Simple Python program to use command line argument to call a function with parameters 
+execution on a command line:
+
+> commandLineFunctionPassing.py conPrint Hello
+> commandLineFunctionPassing.py conPrint "Hello World"
+
+Output:
+
+Hello
+Hello World
+"""
+import sys
+
+# function conPrint to print the 3rd argument as the output
+def conPrint():
+    print(sys.argv[2])
+
+
+# directly calling the 2nd argument as a function using globals()
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        globals()[sys.argv[1]]()
+    else:
+        print("Parameters are not provided")
